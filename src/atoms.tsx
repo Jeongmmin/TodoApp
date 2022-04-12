85bca4f946eee1d48b7c0b43b6b0a9f0ca370087
Jeongmmin <seo88kr@hanmail.net>
2022-04-13 02:44:10 +0900

    ✍ Study Selectors part Two

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -7,6 +7,11 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
 }
 
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
@@ -16,26 +21,18 @@ export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    const category = get(categoryState);
+    // if(category === "TO_DO") return toDos.filter((toDo) => toDo.category === "TO_DO");
+    // if(category === "DOING") return toDos.filter((toDo) => toDo.category === "DOING")
+    // if(category === "DONE") return toDos.filter((toDo) => toDo.category === "DONE");
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
 
-// derived State : state를 입력 받아서 그걸 변형해 반환하는 순수함수를 거쳐 반환된 값을 말함
-
+// 컴포넌트에서 할 수 있지만 selector가 있으면 체계적으로 데이터를 관리할 수 있다.
 /**
- * 현재 : 모든 상태의 todo들이 todos에 들어있다.
+ * 한 번에 한 배열만 보여줄 것이다.
+ * 내가 보고있는 카테고리에 todo 추가할 수 있도록 만들 것이다.
  *
- * 카테고리 별로 분류하고 싶다. -> selector사용
- * 하지만 atom을 3개로 나누고 싶지는 않음, selector function을 사용할 것 (atom의 ouput을 변형시킬 것이다.)
  *
- * selector : atom을 가져다가 output을 변형할 수 있다.
- * get function이 있어야 atom을 받을 수 있다.
- *
- * 구현하고 싶은것 -> 배열을 담은 배열을 return하는 것  -> JS 의 filter function이용 (배열에서 조건에 맞지 않는 원소들을 제거한 배열을 return한다.)
- * 
- * 결과적으로는 카테고리 별로 랜더링 하고 싶음
  */
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,45 +1,30 @@
-import { useRecoilValue } from "recoil";
-import { toDoSelector, toDoState } from "../atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
-
-
-
 function TodoList() {
   // 배열이 return될때
-  const [toDo, doing, done] = useRecoilValue(toDoSelector)
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  // SELECT의 변경을 감지하는 함수
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
+  // console.log(category)
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <h2>To Do</h2>
-      {/* to do list */}
-      <ul>
-        {toDo.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Doing</h2>
-      {/* to do list */}
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Done</h2>
-      {/* to do list */}
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => <ToDo key={toDo.id} {...toDo}/>)}
     </div>
   );
 }
 
 export default TodoList;
-

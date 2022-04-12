08720047172fa5cd609d6fd4f117fb6954129acf
Jeongmmin <seo88kr@hanmail.net>
2022-04-13 01:57:50 +0900

    ✍ Study Selector

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from "recoil";
 
 // 공통적으로 많이 쓰이니까 atom으로 분리
 export interface IToDo {
@@ -7,8 +7,35 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
 }
 
-
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
 });
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  },
+});
+
+// derived State : state를 입력 받아서 그걸 변형해 반환하는 순수함수를 거쳐 반환된 값을 말함
+
+/**
+ * 현재 : 모든 상태의 todo들이 todos에 들어있다.
+ *
+ * 카테고리 별로 분류하고 싶다. -> selector사용
+ * 하지만 atom을 3개로 나누고 싶지는 않음, selector function을 사용할 것 (atom의 ouput을 변형시킬 것이다.)
+ *
+ * selector : atom을 가져다가 output을 변형할 수 있다.
+ * get function이 있어야 atom을 받을 수 있다.
+ *
+ * 구현하고 싶은것 -> 배열을 담은 배열을 return하는 것  -> JS 의 filter function이용 (배열에서 조건에 맞지 않는 원소들을 제거한 배열을 return한다.)
+ * 
+ * 결과적으로는 카테고리 별로 랜더링 하고 싶음
+ */
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -7,16 +7,33 @@ import ToDo from "./ToDo";
 
 
 function TodoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  // 배열이 return될때
+  const [toDo, doing, done] = useRecoilValue(toDoSelector)
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       {/* to do list */}
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      {/* to do list */}
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      {/* to do list */}
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
@@ -25,19 +42,4 @@ function TodoList() {
 }
 
 export default TodoList;
-
-
-// toDo의 category를 수정하려면?
-/**
- * 1) to do가 어디있는지를 알아야 한다. -> id로 todo 찾기 todo의 index찾는 방법만 알면 된다.
- * 
- * 2) new toDo 만들기
- * 
- * 3) targetIndex의 Todo를 new ToDo로 바꾸기
- * 
- * 4) 배열의 원소를 어떻게 교체하는지를 알기
- *    
- *      slice를 사용하면 배열을 자를 수 있다.
- * 
- */
 
